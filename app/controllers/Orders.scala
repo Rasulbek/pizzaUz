e9e2675a696347301efda9b5ce47d709ea87c575
RASULBEK <mr.abdurasuloff@gmail.com>
2015-03-15 07:55:33 +0500

    Case class yaratildi

---
+++ app/controllers/Orders.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc._
+
+/**
+ * Created by 
+    ABDURASULOFF RASULBEK 
+    (mr.abdurasuloff@gmail.com)
+    on 14.03.2015.
+ */
+class Orders extends Controller{
+
+}
