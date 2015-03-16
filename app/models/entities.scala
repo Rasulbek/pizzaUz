cfadfcaf3a22b79c629d4e59102428373a1ea1a7
RASULBEK <mr.abdurasuloff@gmail.com>
2015-03-16 17:25:48 +0500

    Baza bilan boglandi

--- app/models/entities.scala
+++ app/models/entities.scala
@@ -7,8 +7,26 @@ package models
     on 14.03.2015.
  */
 
+import play.api.db.slick.Config.driver.simple._
+
 case class Pizza (id: Option[Int],
                  name: String,
                  desc: String,
                  image: String,
                  price: Int)
+
+class ProductTable(tag: Tag) extends Table[Pizza](tag, "PRODUCTS") {
+
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+
+  def name = column[String]("NAME", O.Default(""))
+
+  def description = column[String]("DESCRIPTION", O.Default(""))
+
+  def image = column[String]("IMAGE", O.Default("logo.png"))
+
+  def price = column[Int]("PRICE", O.Default(0))
+
+  def * = (id.?, name, description, image, price) <> (Pizza.tupled, Pizza.unapply _)
+
+}
--- conf/evalutions.default/2.sql
+++ conf/evalutions.default/2.sql
@@ -3,5 +3,5 @@
 # --- !Ups
 
 INSERT into "PRODUCTS"("NAME", "DESCRIPTION", "IMAGE",  "PRICE") values ('SUPREME','Classic marinara sauce, pepperoni, seasoned pork, beef, fresh mushrooms, fresh green bell peppers and fresh red onions.', 'piz1.png',7000);
-INSERT into "PRODUCTS"("NAME", "DESCRIPTION", "IMAGE", "PRICE") values ('MEAT LOVER''S','Classic marinara sauce, pepperoni, Italian sausage, roasted ham, hardwood smoked bacon, seasoned pork and beef.', 'piz2.png',8500);
-INSERT into "PRODUCTS"("NAME", "DESCRIPTION", "IMAGE", "PRICE") values ('VEGGIE LOVER''S','Classic marinara sauce, fresh mushrooms, fresh red onions, fresh green bell peppers, Roma tomatoes and black olives.', 'piz3.png',9000);
+INSERT into "PRODUCTS"("NAME", "DESCRIPTION", "IMAGE", "PRICE") values ('MEAT LOVER`S','Classic marinara sauce, pepperoni, Italian sausage, roasted ham, hardwood smoked bacon, seasoned pork and beef.', 'piz2.png',8500);
+INSERT into "PRODUCTS"("NAME", "DESCRIPTION", "IMAGE", "PRICE") values ('VEGGIE LOVER`S','Classic marinara sauce, fresh mushrooms, fresh red onions, fresh green bell peppers, Roma tomatoes and black olives.', 'piz3.png',9000);
